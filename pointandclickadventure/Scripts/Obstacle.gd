6cf53cd923e2c996184aeb0d0a845c033c58c275
kyle-stevens <43458322+kyle-stevens@users.noreply.github.com>
2021-05-06 21:36:42 -0700

    Update 2: Movement Fixes and Interactable Prototyping
    Movement:
    +Fixed stationary rotation issues
    
    Interactable:
    +Objects Highlight on Hover
    +Detect Clicks
    -Click detection is poorly implemented
    -Poor Implementation across nodes

--- pointandclickadventure/Scripts/Obstacle.gd
+++ pointandclickadventure/Scripts/Obstacle.gd
@@ -4,7 +4,8 @@ onready var polygon = get_node("Polygon2D")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var hovered = false
+var playerNearby = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,13 +13,24 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if hovered and Input.is_action_just_pressed("move_click") and playerNearby:
+		print("Clicked")
 
 
 func _on_Area2D_mouse_entered():
 	polygon.color = Color(1,0,0,1)
+	hovered = true
 
 
 func _on_Area2D_mouse_exited():
 	polygon.color = Color(1,1,1,1)
+	hovered = false
+
+
+func _on_Area2D2_body_entered(body):
+	playerNearby = true
+
+
+func _on_Area2D2_body_exited(body):
+	playerNearby = false
--- pointandclickadventure/Scripts/Player.gd
+++ pointandclickadventure/Scripts/Player.gd
@@ -2,32 +2,38 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 
-var target = Vector2()
-var velocity = Vector2()
+var target : Vector2 = Vector2()
+var velocity : Vector2 = Vector2()
 onready var line2d = get_node("Line2D")
 onready var sprite = get_node("Polygon2D")
 onready var camera = get_node("Camera2D")
+var stationary : bool = false
+#var i : int = 0
+
 func _ready():
 	target = position
 
-
-	
 func _physics_process(delta):
-	if Input.is_action_pressed("move_click"):
+	if Input.is_action_pressed("move_click") and not stationary:
 		target = get_global_mouse_position()
-	if Input.is_action_pressed("stop_moving"):
+	elif Input.is_action_pressed("move_click") and stationary:
+		sprite.look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("stop_moving"):
 		target = position
 		line2d.set_point_position(1, Vector2(target.x - position.x, target.y - position.y))
+		stationary = true
+	if Input.is_action_just_released("stop_moving"):
+		stationary = false
 	#not currently working :(
 	if Input.is_action_just_released("scroll_zoom_in") and camera.zoom > 0.5:
 		camera.zoom.x -= 0.01
 	if Input.is_action_just_released("scroll_zoom_out") and camera.zoom < 1.5:
 		camera.zoom.y += 0.01
-	
 	velocity = position.direction_to(target) * speed
-	# look_at(target)
+	# look_at(target) and move, issue with this when stationary
 	if position.distance_to(target) > 5:
 		velocity = move_and_slide(velocity)
 		line2d.set_point_position(1, Vector2(target.x - position.x, target.y - position.y))
-		sprite.look_at(target)
-		
+	sprite.look_at(target)
+	#elif stationary:
+	#	sprite.look_at(get_global_mouse_position())
